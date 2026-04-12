8ea70b964a7b00be36ef107cceeb77c23a66fa1e
Dhia ul haq el hakim <dhiahakim6@gmail.com>
2026-04-12 19:48:44 +0700

    Update 01_data_cleaning.sql

--- sql/01_data_cleaning.sql
+++ sql/01_data_cleaning.sql
@@ -1,5 +1,4 @@
 --cek apakah ada data kosong
-
 SELECT
 COUNT(*) AS total_rows,
 COUNTIF(customer_id IS NULL) AS missing_customer_id,
@@ -11,20 +10,20 @@ COUNTIF(quantity IS NULL) AS missing_quantity,
 COUNTIF(total_amount IS NULL) AS missing_total_amount
 FROM `pet_store.sales`;
 
+
 --ternyata ada, cek ada berapa data yang kosong tersebut
 SELECT
 COUNT(*) AS total_transactions,
 COUNTIF(customer_id IS NULL) AS missing_customer
 FROM `pet_store.sales`;
+--ada 9615 transaksi yang tidak ada customernya, 
 
---ada 9615 transaksi yang tidak ada customernya, lihat itu berapa persen dari total transaksi
-
+--lihat itu berapa persen dari total transaksi
 SELECT
 ROUND(
   (COUNT(*) - COUNT(customer_id))*100.0/COUNT(*),2
 ) AS missing_percentage
 FROM `pet_store.sales`;
-
 --4,43% yang kosong customer_id nya. masih dalam tahap wajar, sehingga kita akan biarkan row yang kosong tersebut
 
 --cek apakah hanya terjadi di transaksi tertentu
@@ -42,8 +41,9 @@ SELECT transaction_id, COUNT(*)
 FROM `pet_store.sales`
 GROUP BY transaction_id
 HAVING COUNT(*) > 1;
---ternyata ada banyak transaction_id yang duplikat, identifikasi dulu duplikatnya
+--ternyata ada banyak transaction_id yang duplikat, 
 
+--identifikasi dulu duplikatnya
 SELECT
 transaction_id,
 COUNT(*) AS duplicate_count
@@ -56,7 +56,6 @@ ORDER BY duplicate_count DESC;
 SELECT *
 FROM `pet_store.sales`
 WHERE transaction_id = 15838236;
-
 --During data validation, it was discovered that several transaction_id values were associated with multiple customers and transaction dates. This indicates that the transaction_id field was not globally unique and could represent multiple separate transactions. To resolve this issue, a new transaction key was created using a composite identifier combining transaction_id, customer_id, and transaction_date. This ensured accurate transaction counting and prevented aggregation errors in downstream analysis.
 
 --cek lagi apakah banyak yg seperti ini
@@ -68,8 +67,9 @@ FROM `pet_store.sales`
 GROUP BY transaction_id
 HAVING customers > 1
 OR dates > 1;
---karna muncul banyak row → berarti transaction_id tidak reliable, buat transaction_key sebagai unique transaction baru
+--karna muncul banyak row → berarti transaction_id tidak reliable, 
 
+--buat transaction_key sebagai unique transaction baru
 SELECT *,
 CONCAT(
 transaction_id,'_',
@@ -91,10 +91,9 @@ CAST(transaction_date AS STRING)
 ) AS transaction_key
 FROM `pet_store.sales`
 );
-
 --ternyata tidak unik, dari total 217107 row, hanya ada 124835 yang unik
 
---Cara cek apakah ini memang product-level rows
+--Cek apakah ini memang product-level rows
 SELECT
 transaction_key,
 COUNT(*) AS items_in_transaction
@@ -110,11 +109,9 @@ FROM `pet_store.sales`
 GROUP BY transaction_key
 ORDER BY items_in_transaction DESC
 LIMIT 10;
-
 --After creating a composite transaction_key, it was observed that the number of unique transaction keys was smaller than the total number of rows. This indicates that the dataset is structured at the line-item level, where a single transaction can contain multiple products. Therefore, the transaction_key represents a unique transaction, while each row represents an individual product purchased within that transaction.
 
 --Validate True Duplicate Rows Sekarang kita cek apakah ada row yang benar-benar duplikat.
-
 SELECT
 customer_id,
 transaction_id,
@@ -135,7 +132,6 @@ HAVING COUNT(*) > 1;
 --hasil 0, data is clean
 
 --Check apakah product_id ada yang NULL
-
 SELECT
   COUNT(*) AS null_product_id
 FROM `pet_store.sales`
